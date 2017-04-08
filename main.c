3ea417942b50edd35bf0c7445accbc0b87585ea6
dimuthnc <dimuthu.13@cse.mrt.ac.lk>
2017-04-08 13:28:08 +0530

    linkedlist initializing, adding node or checking membership can be done

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <stdbool.h>
 
 
 typedef struct node {
@@ -7,24 +8,63 @@ typedef struct node {
     struct node * next;
 } node_t;
 
+node_t* createLinkedList(int value){
+    node_t * head = NULL;
+    head = malloc(sizeof(node_t));
+    head->value = value;
+    head->next =NULL;
+    return head;
+}
+
+
+
+void addNode(node_t* head, int value){
+    node_t* current =head;
+    while(current->next!=NULL){
+        current = current->next;
+    }
+    node_t* newNode =  malloc(sizeof(node_t));
+    current->next = newNode;
+    newNode->value = value;
+    newNode->next =NULL;
+}
+
+void printLinkedList(node_t* head){
+    node_t* current =head;
+    while(current->next!=NULL){
+        printf("%d \n",current->value);
+        current = current->next;
+    }
+    printf("%d \n",current->value);
+
+}
+
+bool member(int value,node_t* head){
+    node_t* current =head;
+    while(current->value!=value && current->next!=NULL){
+        current = current->next;
+    }
+    if(current->value==value){
+        return true;
+    }
+    return false;
+
+}
+
 int main() {
 
 
-    node_t * head = NULL;
-    head = malloc(sizeof(node_t));
-    if (head == NULL) {
-        return 1;
-    }
+    node_t * head = createLinkedList(1434);
 
-    head->value = 1;
-    head->next = malloc(sizeof(node_t));
-    head->next->value = 2;
-    head->next->next = NULL;
+    addNode(head,2344);
+    addNode(head,33434);
 
-    printf("%d \n",head->value);
+    printLinkedList(head);
 
-    node_t* current = head->next;
-    printf("%d \n",current->value);
+    printf("%d \n",member(231,head));
+    printf("%d \n",member(33434,head));
+
 
     return 0;
 }
+
